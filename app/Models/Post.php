9903de086a7814f6e87f63d905d65e93d3d6fa60
Ikbal Maulana <86029687+ajobale@users.noreply.github.com>
2024-07-19 10:33:40 +0700

    sebelum debug sendiri

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,30 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
-use PhpParser\Node\Stmt\Static_;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    use HasFactory;
 
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 
-
-
-
-    // public static function find($slug): array
-    // {
-    //     //callback function
-    //     // return Arr::first(static::all(), function ($post) use ($slug) {
-    //     //     return $post['slug'] == $slug;
-    //     // });
-
-    //     $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);;
-
-    //     if (!$post) {
-    //         abort(404);
-    //     }
-
-    //     return $post;
-    // }
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,19 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     
-
+@dd($posts)
     @foreach ($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="posts/{{ $post['slug'] }}" class="mb-3 text-3xl tracking-tight font-bold text-gray-900 hover:text-gray-700">{{ $post['title'] }}</a>
+        <a href="posts/{{ $post['id'] }}" class="mb-3 text-3xl tracking-tight font-bold text-gray-900 hover:text-gray-700">{{ $post['title'] }}</a>
         <div class="text-base text-gray-600 mt-2">
             {{-- kalau array echonya tu $post['title'] --}}
-            <a href="#">{{ $post['author'] }}</a> | 1  January 2024
+            <a href="#"{{ $post->author->name }}</a> | {{ $post->created_at->format('j F Y') }}
 
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
-        <a href="posts/{{ $post['slug'] }}" class="font-medium text-blue-600 hover:underline" >Read More &raquo;</a>
+        <a href="posts/{{ $post['id'] }}" class="font-medium text-blue-600 hover:underline" >Read More &raquo;</a>
     </article>  
     @endforeach
     
-
    
 </x-layout>
